fa46f487e2027332c62a23cb7f0b252344397407
Trupti Khadtare <thankare167@gmail.com>
2025-07-03 23:25:34 +0530

    First Commit

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,14 @@
+import { Briefcase, Code, User } from "lucide-react";
+
+export const AboutSection = () => {
+  return (
+    <section id="about" className="py-24 px-4 relative">
+      {" "}
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          About <span className="text-primary">Me</span>
+        </h2>
+      </div>
+    </section>
+  );
+};
